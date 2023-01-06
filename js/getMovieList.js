5d4da83a191253204e368be525a98c115f208dc9
Oleh020 <89664420+Oleh020@users.noreply.github.com>
2023-01-06 22:09:33 +0200

    Header added and some html restructured (#6)
    * undefined bug in box office block fixed
    
    * basic header added `n some html restructured
    
    * cards overlapse bug fixed
    
    * minor logo fixes
    
    * landing page movie cards section bug fixed
    
    * added error card on fetch false response

--- js/getMovieList.js
+++ js/getMovieList.js
@@ -1,9 +1,58 @@
 import MOVIES_API_KEY from "./key.js";
-import NO_POSTER_URL from "./constants.js"
+import {NO_POSTER_URL} from "./constants.js";
+import {headerRender} from "./headerRender.js";
 
-const movieListById = document.querySelector('#movie-list'),
-     searchBarInput = document.querySelector('.search-bar__input'),
-     searchBtn = document.querySelector('.search-bar__button');
+
+const body = document.querySelector('body');
+//At first function that creates header initializes
+headerRender();
+
+
+/**
+ * This function renders a search bar proceduraly
+ */
+function searchBarRender() {
+    const SBSection = document.createElement('section'),
+         SBForm = document.createElement('form'),
+         SBInput = document.createElement('input'),
+         SBButton = document.createElement('button');
+
+    SBSection.id = 'search-bar';
+    SBForm.classList.add('search-bar__form');
+    SBForm.type = 'submit';
+    SBInput.classList.add('search-bar__input');
+    SBInput.type = 'text';
+    SBInput.placeholder = 'Enter the title of the movie please';
+    SBButton.classList.add('search-bar__button');
+    SBButton.type = 'submit';
+    SBButton.innerHTML = '&#10162';
+
+    SBForm.append(SBInput, SBButton);
+    SBSection.append(SBForm);
+    body.append(SBSection);
+}
+
+searchBarRender();
+
+/**
+ * This function renders a section for movie cards 
+ */
+function renderMovieCardsSection() {
+    const movieCardsSection = document.createElement('section');
+        movieCardsSection.id = 'movie-list';
+    body.append(movieCardsSection);
+}
+renderMovieCardsSection();
+
+/**
+ * This function renders an error card on fetch response === false
+ */
+function noResultsFoundRender() {
+    const errCard = document.createElement('div');
+    errCard.classList.add('movie-list__nothing-found');
+    errCard.innerText = 'No results found, try again';
+    document.querySelector('#movie-list').append(errCard);
+}
 
 /** this function receives an array of movies and render them on page */
 function appendMovieCard(movie) {
@@ -24,7 +73,7 @@ function appendMovieCard(movie) {
         } else {
             movieCard.style.background = `URL(${NO_POSTER_URL}) no-repeat center center/cover`;
         }
-    movieListById.appendChild(movieCard);
+    document.querySelector('#movie-list').appendChild(movieCard);
 }
 
 /** this function sends request to the API and gets the movie list */
@@ -33,14 +82,21 @@ async function getMovieList(page = 1, searchValue='any') {
     const URL = `https://www.omdbapi.com/?s=${searchValue}&page=${page}&apikey=${MOVIES_API_KEY}`;
     const res = await fetch(`${URL}`);
     const movieList = await res.json();
-    movieList.Search.forEach(appendMovieCard);
+    console.log(movieList);
+    console.log(movieList.Response);
+    if (movieList.Response === 'True'){
+        movieList.Search.length = 9;
+        movieList.Search.forEach(appendMovieCard);
+    } else{
+        noResultsFoundRender();
+    }
 }
 
 getMovieList();    
  
 /** this function resets movie list */
 function clearMovieList() {
-    movieListById.innerHTML = ' ';
+    document.querySelector('#movie-list').innerHTML = ' ';
 }
 
 /** this func receives a user input and executes list clearing and rendering funcs */
@@ -49,7 +105,7 @@ function handleMovieSearch(inputValue) {
     getMovieList(1, inputValue);
 }
 
-searchBtn.addEventListener('click', (e) => {
+document.querySelector('.search-bar__form').addEventListener('submit', (e) => {
     e.preventDefault();
-    handleMovieSearch(searchBarInput.value);
+    handleMovieSearch(document.querySelector('.search-bar__input').value);
 });
